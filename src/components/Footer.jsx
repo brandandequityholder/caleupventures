cb59796940b849c8453ff95363f89322aa55e141
suborna27 <suborna27akter@gmail.com>
2024-09-01 15:13:29 +0600

    footer text updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,16 +23,11 @@ function Footer() {
 				</span>
 			</p>
 			<div className='w-fit mx-auto'>
-				<button
-					onClick={() =>
-						window.open('mailto:deals@scaleupventures.co', '_blank')
-					}
-					className='py-[20px] lg:py-[24px] w-[281px] border-t-2 border-b-2 flex justify-between items-center'>
-					<p className='text-[14px] lg:text-[17.39px] leading-[26px] tracking-[2.435px] font-[700]'>
-						CONTACT US
-					</p>
-					{arrow}
-				</button>
+			<p className='text-[17.39px] font-[500] leading-[26px] '>
+			Contact Us:
+				<br />
+				deals@scaleupventures.co
+			</p>
 			</div>
 		</footer>
 	);
